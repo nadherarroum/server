12ae93f462de69113d80be66fbeeb87114141a1e
Nadher Arroum <nadher.arroum@gmail.com>
2022-06-06 15:04:53 +0100

    implement Server service

--- src/main/java/com/nadherarroum/server/service/ServerService.java
+++ src/main/java/com/nadherarroum/server/service/ServerService.java
@@ -2,12 +2,14 @@ package com.nadherarroum.server.service;
 
 import com.nadherarroum.server.model.Server;
 
+import java.io.IOException;
+import java.net.UnknownHostException;
 import java.util.Collection;
 
 public interface ServerService {
 
     Server create(Server server);
-    Server ping(String ipAddress);
+    Server ping(String ipAddress) throws IOException;
     Collection<Server> list(int limit);
     Server get(Long id);
     Server update(Server server);
